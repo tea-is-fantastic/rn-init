344866923a763ccbbbefec44e18bf7443fdb71b7
Asfandyar Sheikh <andy@replyqa.com>
2023-03-20 09:34:52 +0500

    woohoo

--- utils/paths.js
+++ utils/paths.js
@@ -2,7 +2,5 @@ const path = require("path");
 
 const src = path.join(process.env.INIT_CWD, 'mobile');
 const dataPath = path.join(process.env.INIT_CWD, 'data');
-const env = path.join(process.env.INIT_CWD, 'env');
 
-
-module.exports = {src, dataPath, env};
+module.exports = {src, dataPath};
