34dae657e5f7429fca7698144dc01228fb30f2ab
Asfandyar Sheikh <andy@replyqa.com>
2023-03-20 19:31:09 +0500

    woohoo

--- bin/structure.js
+++ bin/structure.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const {values} = require("lodash");
 const {rn_dirs, src, regexReplace, projectDir} = require("../utils");
 const path = require("path");
-const fse = require("fs-extra");
 
 async function fileChanges() {
   await regexReplace(
@@ -13,27 +12,6 @@ async function fileChanges() {
   );
 }
 
-async function fileChanges1() {
-  const code = `
-import React from 'react';
-
-import {AppWrapper} from '@tisf/rn-providers';
-import RootNavigator from './screens/RootNavigator';
-import {AppInfo} from './shared/constants/env';
-
-function App(): JSX.Element {
-  return (
-    <AppWrapper appInfo={AppInfo}>
-      <RootNavigator />
-    </AppWrapper>
-  );
-}
-
-export default App; 
-`;
-  await fse.outputFile(path.join(rn_dirs.rn_src, 'App.tsx'), code);
-}
-
 async function structure() {
   projectDir();
   for (const c of values(rn_dirs)) {
@@ -41,7 +19,6 @@ async function structure() {
   }
   await fs.promises.rename(path.join(src, 'App.tsx'), path.join(rn_dirs.rn_src, 'App.tsx'))
   await fileChanges();
-  await fileChanges1();
 }
 
 module.exports = structure;
